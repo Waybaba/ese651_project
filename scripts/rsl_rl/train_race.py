eba49256d1d2b87aaa1a9e04810e42541385ed7c
waybaba <wwang828@uwo.ca>
2025-10-17 22:23:44 -0400

    Adjust reward scales in training script: decrease progress goal reward scale from 50.0 to 5.0 and increase death cost from -10.0 to -100.0 for improved reward dynamics.

--- scripts/rsl_rl/train_race.py
+++ scripts/rsl_rl/train_race.py
@@ -107,9 +107,9 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
 
     # TODO ----- START ----- Define rewards scales
     # reward scales
-    progress_goal_reward_scale = 50.0
+    progress_goal_reward_scale = 5.0
     crash_reward = 0.
-    death_cost = -10.0
+    death_cost = -100.0
 
     rewards = {
         'progress_goal_reward_scale': progress_goal_reward_scale,
